4c37091e663b624df6b17bef094bb852482eb93c
kambletejas271-source <kambletejas271@gmail.com>
2026-04-30 12:14:57 +0530

    Add files via upload

---
+++ ex 9.3.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 16:41:19 2026
+
+@author: User
+"""
+
+import math
+
+x1 = float (input ("Enter x1: "))
+
+y1 = float (input("Enter y1: "))
+
+x2 = float(input ("Enter x2: "))
+
+y2 = float(input("Enter y2: "))
+
+distance = math.sqrt ((x2 - x1)**2 + (y2 - y1)**2)
+
+print("Distance between two points:", distance)
